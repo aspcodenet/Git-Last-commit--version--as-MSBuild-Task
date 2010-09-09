70a8c1fea87c87ed2ab0d1a1edbb8b9b26d9b48f
Stefan <stefan.holmberg@systementor,se>
2010-09-09 13:35:07 +0200

    Initial commit

---
+++ GitVersion/GitVersion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+/*
+ * 
+ * LICENSE MIT
+ * 
+ * (C) Stefan Holmberg Systementor AB http://www.aspcode.net
+ * 
+ */
+
+namespace GitVersion
+{
+    public class GitVersion : Task
+    {
+
+        public GitVersion()
+        {
+            LocalPath = ".";
+        }
+
+        public string LocalPath { get; set; }
+
+
+        [Output]
+        public string LastCommit { get; set; }
+        public override bool Execute()
+        {
+            System.Diagnostics.ProcessStartInfo oInfo = new System.Diagnostics.ProcessStartInfo("git.exe", " log --oneline -1");
+            oInfo.UseShellExecute = false;
+            oInfo.ErrorDialog = false;
+            //oInfo.CreateNoWindow = true;
+            oInfo.CreateNoWindow = false;
+            oInfo.WorkingDirectory = LocalPath;
+            oInfo.RedirectStandardOutput = true;
+
+            try
+            {
+                Process p = System.Diagnostics.Process.Start(oInfo);
+                p.WaitForExit();
+
+                System.IO.StreamReader oReader2 = p.StandardOutput;
+                string sRes = oReader2.ReadToEnd();
+                oReader2.Close();
+                LastCommit = sRes.Split(' ')[0];
+                return true;
+                //
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+            return false;
+
+        }
+    }
+}
